63ef2afa0077c35bb66a11656b14bb5fa4a1122c
js <xjs.js@outlook.com>
2019-09-13 10:23:03 +0800

    Updates

---
+++ precache-manifest.6321577df10cdefac585b21cfe0fb519.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5aae4438554dc245b8da2353fffe43a",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c7ffd47e73350b3cd40b",
+    "url": "/react-tic-tac-toe/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "bdf318e90a08eec2549a",
+    "url": "/react-tic-tac-toe/static/js/2.a8d6398c.chunk.js"
+  },
+  {
+    "revision": "c7ffd47e73350b3cd40b",
+    "url": "/react-tic-tac-toe/static/js/main.0057c17b.chunk.js"
+  },
+  {
+    "revision": "084395ae5a1556397b3d",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.1634a412.js"
+  }
+]);
